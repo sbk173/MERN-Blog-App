999a319d9ce3320dedcd04c1e5247a73e4a06cca
SaadBK <saadbk173@gmail.com>
2023-03-02 20:35:16 +0530

    tried pdf-viewer

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom'
 import LoginForm from './Login';
 import Home from './Home';
-import RegistrationForm
- from './Register';
+import RegistrationForm from './Register';
+import ArticleView from './ArticleView';
+
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<LoginForm/>}/>
       <Route path='/register' element={<RegistrationForm/>}/>
+      <Route path='/article' element = {<ArticleView path='../public/DDCO_ASSIGNMENTS.pdf'/>}/>
     </Routes>
 
   )
